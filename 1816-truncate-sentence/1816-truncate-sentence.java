dc8c9862d175f7e6a67ec8533be192c6e736a980
rbd1411 <96679588+rbd1411@users.noreply.github.com>
2026-09-23 14:55:49 +0530

    Time: 0 ms (100%), Space: 42.9 MB (81.35%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        int c=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==' '){
+                c+=1;
+            }
+            if(c==k){
+                return s.substring(0,i);
+            }
+        }
+        return s;
+    }   
+}
